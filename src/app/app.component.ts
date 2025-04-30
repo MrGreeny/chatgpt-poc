39bcd602314f05f41e90c0c7c8422e1e54bc77ac
SAVOV Stefan <stefan.savov@soprasteria.com>
2025-04-30 14:06:32 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'chatgpt-poc';
+  userInput = '';
+  response = '';
+
+  constructor(private http: HttpClient) {}
+
+  sendMessage() {
+    this.http.post<any>('/api/chat', { message: this.userInput })
+      .subscribe(res => {
+        this.response = res.choices[0].message.content;
+      });
+  }
 }
